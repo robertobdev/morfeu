53010bdfc16dac669fba55aa019d1b21f50d0787
Roberto Barbosa <robertojf95@gmail.com>
2021-01-20 16:39:36 -0300

    Update selectBuilder.js

--- src/selectBuilder.js
+++ src/selectBuilder.js
@@ -1,12 +1,14 @@
-const selectBuilder = (tables) => {
+const selectBuilder = (tables, ids) => {
   let sqlSelect = ``;
   for (const table of tables) {
     sqlSelect += `SELECT 
-      distinct ${table}.*    
+        distinct ${table.alias}.*    
       FROM
         users
       LEFT JOIN
-        user_types ON users.user_type_id = user_types.id; `;
+        user_types ON users.user_type_id = user_types.id
+      WHERE
+      ${table.alias}.id IN (${ids})`;
   }
   return sqlSelect;
 };
